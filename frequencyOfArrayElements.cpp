1ae596279179918772bfdaf9152b580dbc79dc09
Akshat <42962482+Akshat111999@users.noreply.github.com>
2020-05-18 00:23:32 +0530

    find out the occurence of unique elements

---
+++ frequencyOfArrayElements.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[100], freq[100], i,j,n,count=0;
+    cout<<"enter no of elements\n";
+    cin>>n;
+    cout<<"enter the elements in array\n";
+    for (i=0; i<n;i++){
+        cin>>arr[i];
+    }
+    for (i=0;i<n;i++){
+        count=1;
+        if (arr[i]!=-1){
+            for (j=i+1; j<n; j++){
+                if (arr[i]==arr[j]){
+                    count++;
+                    arr[j]=-1;
+                }
+            }
+            freq[i]=count;
+        }
+    }
+    for(i=0; i<n; i++){
+        if (arr[i]!=-1){
+            cout<<"\nfrequency of element "<<arr[i]<<" is "<<freq[i];
+        }
+    }
+    return 0;
+}
